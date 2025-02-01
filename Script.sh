b227e8ff0ff4f7af5c5470c2947c8a699070169f
pavan <pavankumar49210@gmail.com>
2025-02-01 19:55:58 +0530

    practice

--- Script.sh
+++ Script.sh
@@ -8,4 +8,20 @@ then
     exit
 dnf install mysql
 
+if [ $? -ne 0 ]
+then
+   echo "Installing mysql...failure"
+   exit
+else
+   echo "Installing mysql...SUCCESS"
+fi
+
+dnf install git 
+
+if [ $? -ne 0 ]
+then
+    echo "Installing Git...FAILURE"
+    exit
+else
+    echo "Installing Git...SUCCESS"
 fi
