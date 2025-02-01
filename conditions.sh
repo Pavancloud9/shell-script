99357a672c0233f5a2fbe4368300149e188ad86a
pavan <pavankumar49210@gmail.com>
2025-02-01 15:36:27 +0530

    practice

--- conditions.sh
+++ conditions.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-NUMBER=600
+NUMBER=100
 
-if [ $NUMBER -gt 800 ]
-
-then
-    echo "given number is  greather than $NUMBER"
-else
-    echo "given number is less than or equal to $NUMBER"
-
-date=$(date)
-
-echo "time stamp is $date"
-
-fi
+if 
