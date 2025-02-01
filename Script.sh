1993f09db2503941f24be41f3bf2d91c125dcd09
pavan <pavankumar49210@gmail.com>
2025-02-01 20:09:06 +0530

    practice

--- Script.sh
+++ Script.sh
@@ -4,26 +4,26 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-    echo "ERROR: you must have sudo access to execute this script"
+    echo "ERROR: You must have SUDO access"
     exit
 fi
 
 dnf install mysql
 
 if [ $? -ne 0 ]
-then
-   echo "Installing mysql...failure"
-   exit
-else
-   echo "Installing mysql...SUCCESS"
-fi
-
-dnf install gitggg 
-
-if [ $? -ne 0 ]
-then
-    echo "Installing Git...FAILURE"
+then 
+    echo "Installing mysql...FAILURE"
     exit
 else
-    echo "Installing Git...SUCCESS"
+    else "Installing mysql....SUCCESS"
+fi
+
+dnf install git
+
+if [ $? -ne 0 ]
+then 
+    echo "Installing git...FAILURE"
+    exit
+else
+   echo "installing git success"
 fi
