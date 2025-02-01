e971738342c90e68f1380cc29aec91b457caa470
pavan <pavankumar49210@gmail.com>
2025-02-01 16:02:21 +0530

    practice

--- arrays.sh
+++ arrays.sh
@@ -2,8 +2,8 @@
 
 NAMES=("sai" "pavan" "arjun")
 
-echo "First name: ${NAME=[0]}"
-echo "second name: ${NAME[1]}"
-echo "Third name: ${NAME[3]}"
+echo "First name: ${NAMES=[0]}"
+echo "second name: ${NAMES[1]}"
+echo "Third name: ${NAMES[3]}"
 
 echo "All names are: ${NAMES[@]}"
