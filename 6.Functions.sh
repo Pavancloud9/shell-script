416124c3996a95eb2afa86be75fef01b6e04f427
pavan <pavankumar49210@gmail.com>
2025-02-03 15:40:46 +0530

    added

---
+++ 6.Functions.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+VALIDATE(){
+          if [ $1 -ne 0 ]
+        then
+          echo "$2...FAILURE"
+          exit
+        else
+          echo "$2...SUCCESS"
+      fi
+}
+
+if [ $USERID -ne 0 ]
+   then
+   echo "ERROR: You must have sudo access to execute"
+   exit
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    dnf install mysql
+    VALIDATE $? "Installing mysql"
+else 
+   echo "mysql is already...INSTALLED"
+fi
+
+#################################
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+    dnf install git
+    VALIDATE $? "Installing git"
+else
+   echo "Git is already...installed"
+fi
