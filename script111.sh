016ce3b85365ba8abd81dc0220ff40a0f1dbd7b0
pavan <pavankumar49210@gmail.com>
2025-02-03 11:45:29 +0530

    added

--- script111.sh
+++ script111.sh
@@ -3,21 +3,9 @@
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
-then 
-    echo "ERROR: you must have sudo access to execute"
-    exit
-fi
+   then
+   echo "ERROR: You must have sudo access to execute"
+   exit
 
 dnf list installed mysql
 
-if [ $? -ne 0 ]
-then 
-  dnf install mysql
-
-if [ $? -ne 0 ]
-then 
-   echo "installing mysql...FAILURE"
-   exit
-else
-   echo "installing mysql...SUCCESS"
-fi
