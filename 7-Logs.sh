d3e10cc2c0cc5873c01438bc4dc4efb726c09c5b
pavan <pavankumar49210@gmail.com>
2025-02-06 11:44:32 +0530

    added

--- 7-Logs.sh
+++ 7-Logs.sh
@@ -4,6 +4,8 @@ USERID=$(id -u)
 
 LOGS_FOLDER="/var/log/shellscripts.logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 VALIDATE(){
        if [ $1 -ne 0 ]
