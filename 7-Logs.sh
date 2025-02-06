d961ab2779578178b13ea41ead02e6e74f7b0cd7
pavan <pavankumar49210@gmail.com>
2025-02-06 10:44:37 +0530

    added

---
+++ 7-Logs.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+LOGS_FOLDER="/var/log/shellscripts.logs"
+
+VALIDATE(){
+       if [ $1 -ne 0 ]
+   then
+       echo "$2...FAILURE"
+            exit
+   else
+       echo "$2...SUCCESS"
+fi
+}
+
+if [ $USERID -ne 0 ]
+   then
+   echo "ERROR: You must have sudo access to execute"
+   exit
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    dnf install mysql
+    VALIDATE $? "installing mysql"
+else 
+   echo "mysql is already...INSTALLED"
+fi
+
+#################################
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+    dnf install git
+    VALIDATE $? "installing git"
+else
+   echo "Git is already...installed"
+fi
