0cc84272deb0036b7054e0c05de6cc8abc22d4a4
pavan <pavankumar49210@gmail.com>
2025-02-10 17:04:24 +0530

    added

--- 9-logs.sh
+++ 9-logs.sh
@@ -3,7 +3,7 @@
 USERID=$(id -u)
 
 LOGS_FOLDER="/var/log/shellscripts.logs"
-LOG_FILE=$(echo $0 | cut -d "." -f1)
+LOG_FILE=$( echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
