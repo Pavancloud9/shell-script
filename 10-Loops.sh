995c3aefc7c2360260ee1b04e4fd8d4a0aeaea85
pavan <pavankumar49210@gmail.com>
2025-02-10 19:17:47 +0530

    added

---
+++ 10-Loops.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+LOGS_FOLDER="/var/log/shellscripts.logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+if [ $1 -ne 0 ]
+then 
+   echo "$2...FAILURE"
+   exit
+else
+   echo "$2...SUCCESS"
+fi
+}
+
+echo "script started exectuing at $TIMESTAMP" &>>$LOG_FILE_NAME 
+
+if [ $USERID -ne 0 ]
+then
+   echo "ERROR: You must have sudo access"
+   exit
+fi
+
+####################################################
+
+dnf list installed mysql  &>>$LOG_FILE_NAME
+if [ $? -ne 0 ]
+then
+dnf install mysql  &>>$LOG_FILE_NAME
+  VALIDATE $? "Installing mysql"
+else
+   echo "Installing MYSQL...Already Installed"
+fi
+
+###############################################
+
+dnf list installed git  &>>$LOG_FILE_NAME
+
+if [ $? -ne 0 ]
+then
+dnf install git
+  VALIDATE $? "Installing Git"
+else
+   echo "Installing Git..Already installed"
+fi
+
+for package in $@
+do
+   dnf list installed $package &>>$LOG_FILE_NAME
+   if [ $? -ne 0 ]
+then
+   dnf install $package &>>$LOG_FILE_NAME
+   VALIDATE $? "installing $package"
+else 
+    echo "$package is already Installed"
+done
