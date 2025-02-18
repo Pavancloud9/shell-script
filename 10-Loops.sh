6b272c29acea071922cd07424b0c9b674beeb2a0
pavan <pavankumar49210@gmail.com>
2025-02-18 09:47:15 +0530

    added

--- 10-Loops.sh
+++ 10-Loops.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 USERID=$(id -u)
 
@@ -6,6 +7,9 @@ LOGS_FOLDER="/var/log/shellscripts.logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+
+
 
 VALIDATE(){
 if [ $1 -ne 0 ]
---
+++ 13-practice.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+USERIDDD=$(id -u)
+
+LOGS_FOLDER="/var/log/shellscripts.logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+
+
+VALIDATE(){
+  if [ $? -ne 0 ]
+then
+   echo "$1...Failure"
+   exit
+else
+   echo "$2...Success"
+fi
+}
+
+
+if [ $USERIDDD -ne 0 ]
+then
+   echo "ERROR: You must have SUDO access"
+   exit
+fi
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+   dnf install mysql -y
+   VALIDATE $? "Installing mysql"
+else
+   echo "Already mysql....Installed"
+fi
+
+
+dnf list installed git
+if [ $? -ne 0 ]
+then
+   dnf install git
+   VALIDATE $? "Installing git"
+else
+   echo "Already Git....Installed"
+fi
