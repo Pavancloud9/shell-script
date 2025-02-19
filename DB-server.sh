2c9072488690dbfac52808697658479a271b55d7
pavan <pavankumar49210@gmail.com>
2025-02-19 12:14:16 +0530

    expense-shell

---
+++ DB-server.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+USERIDDD=$(id -u)
+
+LOGS_FOLDER="/var/log/expense-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+  if [ $? -ne 0 ]
+then
+   echo "$1...Failure"
+   exit
+else
+   echo "$2...Success"
+fi
+}
+
+CHECK_ROOT(){
+if [ $USERIDDD -ne 0 ]
+then
+   echo "ERROR: You must have SUDO access"
+   exit
+fi
+}
+
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME 
+
+dnf install mysql-server -y &>>$LOG_FILE_NAME
+VALIDATE $? "Installing mysql"
+
+systemctl enable mysqld &>>$LOG_FILE_NAME
+VALIDATE $? "enabling mysql server"
+
+systemctl start mysqld &>>$LOG_FILE_NAME
+VALIDATE $? "starting mysql server"
+
+mysql -h pavancloud9.online -u root -pExpenseApp@1
