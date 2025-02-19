45cd47bf5eb5c616f9988f1767433a88499202cc
pavan <pavankumar49210@gmail.com>
2025-02-19 19:26:23 +0530

    expense-shell

---
+++ Backend.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+USERIDDD=$(id -u)
+
+LOGS_FOLDER="/var/log/expense-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+  if [ $? -ne 0 ]
+then
+   echo "$1...Failure"
+   exit
+else
+   echo "$2...Success"
+fi
+}
+
+CHECK_ROOT(){
+if [ $USERIDDD -ne 0 ]
+then
+   echo "ERROR: You must have SUDO access"
+   exit
+fi
+}
+
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+dnf module disable nodejs -y &>>$LOG_FILE_NAME
+VALIDATE $? "disabling existing nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE_NAME
+VALIDATE $? "enabling nodejs 20"
+
+dnf install nodejs -y &>>$LOG_FILE_NAME
+VALIDATE "Installing nodejs"
+
+useradd expense &>>$LOG_FILE_NAME
+VALIDATE "adding expense user"
+
+mkdir /app &>>$LOG_FILE_NAME
+VALIDATE "creating app directory"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE_NAME
+VALIDATE $? "Downloading backend"
+
+cd /app
+
+unzip /tmp/backend.zip &>>$LOG_FILE_NAME
+VALIDATE $? "unzipping backend"
+
+npm install &>>$LOG_FILE_NAME
+VALIDATE $? "installing dependencies"
+
+cp /home/ec2-user/shell-script/backend.service /etc/systemd/system/backend.service
+
+#prepare mysql schema
+
+dnf install mysql -y &>>$LOG_FILE_NAME 
+VALIDATE $? "installing mysql client"
+
+mysql -h mysql.pavancloud9.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE_NAME
+VALIDATE $? "setting up transcations schema and tables"
+
+systemctl daemon-reload &>>$LOG_FILE_NAME
+VALIDATE $? "daemon-reload"
+
+systemctl enable backend &>>$LOG_FILE_NAME
+VALIDATE $? "enabling backend"
+
+systemctl restart backend &>>$LOG_FILE_NAME
+VALIDATE $? "restarting backend"
