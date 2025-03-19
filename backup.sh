48218f2a198fbebb5f0004cea3d8822cc5f31808
pavan <pavankumar49210@gmail.com>
2025-03-19 08:47:05 +0530

    added

--- backup.sh
+++ backup.sh
@@ -47,3 +47,10 @@ fi
 FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)    
 
 echo "Files are: $FILES"
+
+if [ -n $FILES ]
+then
+   echo "Files are: $FILES"
+else
+   echo "no files to zip"
+fi
